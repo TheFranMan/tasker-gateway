c63abbb7e41094e4fae6f73dc34470166442cdb6
Francis Dolan <francis.dolan@gmail.com>
2024-10-13 10:56:50 +0100

    Added token validation

--- common/validation.go
+++ common/validation.go
@@ -6,7 +6,12 @@ import (
 )
 
 var regexID = regexp.MustCompile(`^[1-9]+[0-9]*$`)
+var regexToken = regexp.MustCompile(`^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$`)
 
 func ValidID(id int) bool {
 	return regexID.MatchString(strconv.Itoa(id))
+}
+
+func ValidToken(token string) bool {
+	return regexToken.MatchString(token)
 }
--- common/validation_test.go
+++ common/validation_test.go
@@ -24,4 +24,23 @@ func Test_validation(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("token", func(t *testing.T) {
+		for _, test := range []struct {
+			want  bool
+			token string
+		}{
+			{false, ""},
+			{false, "nope"},
+			{false, "acea7232-8a15-44a8-9808-7bf178b3cc5"},   // Too short
+			{false, "acea7232-8a15-44a8-9808-7bf178b3cc566"}, // Too long
+			{false, "acea72328a1544a898087bf178b3cc56"},
+
+			{true, "acea7232-8a15-44a8-9808-7bf178b3cc56"},
+		} {
+			t.Run(test.token, func(t *testing.T) {
+				require.Equal(t, test.want, ValidToken(test.token))
+			})
+		}
+	})
 }
