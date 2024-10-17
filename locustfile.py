3f4cd80a7c0a0c853d3ef5cbe45e3a89281eddca
Francis Dolan <francis.dolan@gmail.com>
2024-10-17 14:22:56 +0100

    Updated load test

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,13 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_throughput
 
-class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("/status/7fbef510-e37d-4884-97e2-c31fac6a89ae", headers={"Authorization": "auth-token-valid-1"})
+class StatusEndpointUser(HttpUser):
+    wait_time = constant_throughput(100)
+    fixed_count = 1000
+
+    @task(10)
+    def token_exists(self):
+        self.client.get("/status/e96b72b8-fe24-46b8-8525-280fac1032fd", headers={"Authorization": "auth-token-valid-1"}, name="status")
+
+    @task(1)
+    def token_doeas_not_exist(self):
+        self.client.get("/status/7fbef510-e37d-4884-97e2-c31fac6a89aa", headers={"Authorization": "auth-token-valid-1"}, name="status")
