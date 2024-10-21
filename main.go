89e930b135cf307c56ab3393e0dc7d6b30e15709
Francis Dolan <francis.dolan@gmail.com>
2024-10-21 11:43:06 +0100

    Updated server port on local dev

--- main.go
+++ main.go
@@ -39,6 +39,17 @@ func main() {
 	log.WithField("ttl", config.RedisKeyTtl).Info("Connecting to Cache")
 	app.Cache = cache.New(config)
 
-	log.WithField("Port", app.Config.Port).Info("Starting server")
-	panic(http.ListenAndServe(fmt.Sprintf(":%d", app.Config.Port), server.New(app)))
+	port := getServerPort(app.Config)
+	log.WithField("Port", port).Info("Starting server")
+	panic(http.ListenAndServe(port, server.New(app)))
+}
+
+// Use localhost:port for local dev to stop the "accept incoming conections" notification popup
+func getServerPort(config *common.Config) string {
+	port := fmt.Sprintf(":%d", config.Port)
+	if config.IsLocal {
+		port = "localhost" + port
+	}
+
+	return port
 }
