69bd39e9493394aecb9a67b24b5156d1d5f07552
Francis Dolan <francis.dolan@gmail.com>
2024-10-21 23:25:47 +0100

    Updated how the server port is formatted

--- main.go
+++ main.go
@@ -39,17 +39,6 @@ func main() {
 	log.WithField("ttl", config.RedisKeyTtl).Info("Connecting to Cache")
 	app.Cache = cache.New(config)
 
-	port := getServerPort(app.Config)
-	log.WithField("Port", port).Info("Starting server")
-	panic(http.ListenAndServe(port, server.New(app)))
-}
-
-// Use localhost:port for local dev to stop the "accept incoming conections" notification popup
-func getServerPort(config *common.Config) string {
-	port := fmt.Sprintf(":%d", config.Port)
-	if config.IsLocal {
-		port = "localhost" + port
-	}
-
-	return port
+	log.WithField("Port", app.Config.Port).Info("Starting server")
+	panic(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), server.New(app)))
 }
