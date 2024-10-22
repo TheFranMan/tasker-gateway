3bd6f46ca0d6f661b979741ef07457b246deceda
Francis Dolan <francis.dolan@gmail.com>
2024-10-22 16:15:19 +0100

    Added API subroute. Added json middleware

--- server/server.go
+++ server/server.go
@@ -23,13 +23,16 @@ func New(app *application.App) *Server {
 	auth := middleware.NewAuth(app.Config)
 	monitor := middleware.NewMonitor(app)
 
-	r.Use(auth.Guard)
-	r.Use(monitor.Record)
-
 	r.Handle("/metrics", promhttp.Handler())
 	r.HandleFunc("/heartbeat", func(w http.ResponseWriter, r *http.Request) {})
-	r.HandleFunc("/user", h.UserDelete).Methods(http.MethodDelete)
-	r.HandleFunc("/status/{token}", h.Status).Methods(http.MethodGet)
+
+	api := r.PathPrefix("/api").Subrouter()
+	api.Use(middleware.Json)
+	api.Use(auth.Guard)
+	api.Use(monitor.Record)
+
+	api.HandleFunc("/user", h.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/status/{token}", h.Status).Methods(http.MethodGet)
 
 	return &Server{
 		router: r,
