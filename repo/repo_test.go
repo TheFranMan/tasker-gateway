62794a8bec720e56abdfc83e48f05bfc456c8ead
Francis Dolan <francis.dolan@gmail.com>
2024-10-25 16:07:30 +0100

    Added repo unit tests

--- repo/repo_test.go
+++ repo/repo_test.go
@@ -1,17 +1,25 @@
 package repo
 
-// func Test_can_get_status_as_a_string(t *testing.T) {
-// 	for _, test := range []struct {
-// 		status types.RequestStatus
-// 		want   types.RequestStatusString
-// 	}{
-// 		{types.RequestStatusNew, types.RequestStatusStringNew},
-// 		{types.RequestStatusInProgress, types.RequestStatusStringInProgress},
-// 		{types.RequestStatusCompleted, types.RequestStatusStringCompleted},
-// 		{types.RequestStatusFailed, types.RequestStatusStringFailed},
-// 	} {
-// 		t.Run(fmt.Sprintf("%d", test.status), func(t *testing.T) {
-// 			require.Equal(t, test.want, getRequestStatusString(test.status))
-// 		})
-// 	}
-// }
+import (
+	"fmt"
+	"testing"
+
+	"github.com/TheFranMan/tasker-common/types"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_can_convert_request_status_to_string(t *testing.T) {
+	for _, test := range []struct {
+		status types.RequestStatus
+		want   types.RequestStatusString
+	}{
+		{types.RequestStatusNew, types.RequestStatusStringNew},
+		{types.RequestStatusInProgress, types.RequestStatusStringInProgress},
+		{types.RequestStatusFailed, types.RequestStatusStringFailed},
+		{types.RequestStatusCompleted, types.RequestStatusStringCompleted},
+	} {
+		t.Run(fmt.Sprintf("%v", test.status), func(t *testing.T) {
+			require.Equal(t, test.want, getRequestStatusString(test.status))
+		})
+	}
+}
